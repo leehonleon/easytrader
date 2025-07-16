9a99dcf132956365cfcd5d715edf04f637eb4bb6
lihongliang <lihl@march-solutions.com>
2025-07-16 10:36:58 +0800

    测试购买ST

---
+++ testbuyst.py
@@ -0,0 +1,11 @@
+import easytrader
+from easytrader.joinquant_follower import JoinQuantFollower
+from easytrader.universal_clienttrader import UniversalClientTrader
+
+import signal
+import sys
+
+user: UniversalClientTrader = easytrader.use('universal_client')
+user.connect(r'C:\\Apps\\同花顺软件\\同花顺\\xiadan.exe')
+user.enable_type_keys_for_editor()
+user.buy('sh000736', 4.30, 1000)
