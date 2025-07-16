bfffdc2fd757a20f5efce4153164edb3b0a659c1
lihongliang <lihl@march-solutions.com>
2025-07-16 10:36:15 +0800

    实例跟单joinquant

---
+++ main.py
@@ -0,0 +1,13 @@
+import easytrader
+import win32api
+user = easytrader.use('universal_client')
+user.connect(r'C:\\Apps\\同花顺软件\\同花顺\\xiadan.exe')
+user.enable_type_keys_for_editor()
+target = 'jq'  # joinquant
+follower = easytrader.follower(target)
+follower.login(user='18698607330', password='47IuoBux')
+follower.follow(user, [
+        #    'https://www.joinquant.com/algorithm/live/index?backtestId=d0db5b91d613f2216af177f29e895ab9', # 小市值
+        'https://www.joinquant.com/algorithm/live/index?backtestId=88fb5e6f325440def3e7589c5f8881dd', # ST股选股弱转强国九条筛选
+        'https://www.joinquant.com/algorithm/live/index?backtestId=838d4b59b63eb1098c68389596eb4c68' # ETF套利小鸡吃米V2.1-Clone-Clone-模拟交易
+        ])
