804c08e6c67ae5db9af88edaea2e9f96dfffec10
lihongliang <lihl@march-solutions.com>
2025-07-22 10:35:25 +0800

    refactor(main): 调整市价委托设置和添加滑点参数
    - 注释掉市价委托设置，以便使用默认委托方式
    - 在 follower.login 中添加 slippage 参数，设置滑点为 0.5%

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import easytrader
 user = easytrader.use('universal_client')
 user.connect(r'C:\\Apps\\同花顺软件\\同花顺\\xiadan.exe')
 user.enable_type_keys_for_editor() # 同花顺需要使用仿真输入
-user.setEntrust('market') # 启动市价委托
+# user.setEntrust('market') # 启动市价委托
 target = 'jq'  # joinquant
 follower = easytrader.follower(target)
 follower.login(user='18698607330', password='47IuoBux')
@@ -13,6 +13,7 @@ follower.follow(user, [
         'https://www.joinquant.com/algorithm/live/index?backtestId=6965531d80c487ccca690ea8b0f221a7', # ETF套利小鸡吃米V2.2
         'https://www.joinquant.com/algorithm/live/index?backtestId=ef67b2e3ba8abff1391985ba867fd12f' # 小市值抽取其他 v1
         ],
-        request_timerange=[("09:00", "11:35"), ("12:50", "15:00")]
+        request_timerange=[("09:00", "11:35"), ("12:50", "15:00")],
+        slippage=0.005,
 )
 print("程序执行完了")
