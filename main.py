4c1eed9f32dff25fcee9395752755639822a6d13
lihongliang <lihl@march-solutions.com>
2025-08-06 14:10:26 +0800

    refactor: 更新 QMT 客户端路径和策略链接
    - 更新 miniqmt 安装路径为 "D:\\Apps\\国金QMT交易端模拟\\userdata_mini"
    - 更新 ST 股选股策略链接

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ target = 'jq'  # joinquant
 follower = easytrader.follower(target)
 follower.login(user='18698607330', password='47IuoBux')
 follower.follow(user, [
-        'https://www.joinquant.com/algorithm/live/index?backtestId=88fb5e6f325440def3e7589c5f8881dd', # ST股选股弱转强国九条筛选
+        'https://www.joinquant.com/algorithm/live/index?backtestId=5719b2bfc4f0d2303000da0db5ab73d4', # ST股选股弱转强国九条筛选
         'https://www.joinquant.com/algorithm/live/index?backtestId=6c4dac368821a5d4dedbd9e020b9dc96', # ETF套利小鸡吃米V2.2
         'https://www.joinquant.com/algorithm/live/index?backtestId=88de576631c6b9765f5d31a9393b8c32' # 小市值抽取其他 v1
         ],
